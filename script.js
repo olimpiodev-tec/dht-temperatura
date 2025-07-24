8aa6571ca7156a9619825da4aea4e8dd7b2295a1
Moisés <olimpiodev.tec@gmail.com>
2025-07-24 10:02:02 -0300

    atualizando

--- script.js
+++ script.js
@@ -5,19 +5,26 @@ clienteWeb = new Paho.MQTT.Client('broker.hivemq.com', 8884, clientId);
 
 // Obtem acesso ao elemento do HTML
 const tempPagina = document.getElementById("temperatura")
+const umidadePagina = document.getElementById("umidade")
+const timePagina = document.getElementById("time")
 
 clienteWeb.onMessageArrived = function(message) {
     const payload = message.payloadString;
     const dados = JSON.parse(payload)
 
     tempPagina.textContent = String(dados.temperatura) + " °C"
+    umidadePagina.textContent = String(dados.umidade) + " %"
+    
+    const agora = new Date()
+    const dataPtBR = agora.toLocaleString()
+    timePagina.textContent = "Atualizado em: " + dataPtBR
 }
 
 clienteWeb.connect({   
     useSSL: true, 
     onSuccess: function() {
         alert('A conexão com Broker foi bem sucedida')
-        clienteWeb.subscribe('senai510/temperatura');
+        clienteWeb.subscribe('senai510/temperatura/m');
     },
     onFailure: function() {
         alert('A conexão com Broker falhou')
